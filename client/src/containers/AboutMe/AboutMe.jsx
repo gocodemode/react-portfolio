53c18bb4e958721f5cadb2246241fbf7cef6177c
Gregory Monroe II <gregthemusician@gmail.com>
2020-08-07 21:00:59 -0400

    moved Images folder. creating style.css in each containers folder

--- client/src/containers/AboutMe/AboutMe.jsx
+++ client/src/containers/AboutMe/AboutMe.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
+import "./AboutMe.css";
+
 
 class AboutMe extends Component {
     render() {
         return (
             <div class="container-fluid">
+                <br />
                 <div class="row">
                     <div class="col-sm-12">
                         <h2 class="top">About Me</h2>
                     </div>
                 </div>
-                <br />
+                <hr />
                 <div class="row">
-                    <div class="col-sm-5">
-                        <img src="../Images/Gregory.jpg" alt="Gregory Monroe II" class="img-thumbnail" />
+                    <div class="col-sm-3">
+                        <img src="/Images/Gregory.jpg" alt="Gregory Monroe II" class="img-thumbnail Gregory" />
                     </div>
-                    <div class="col-sm-7">
+                    <div class="col-sm-5">
                         <p><strong>
-                            Gregory Monroe II is currently enrolled in the Full-Stack Developer Coding Program at Georgia Institute of Technology. Previous to that, he has been playing the piano for almost 25 years. With international private study, Gregory has gone on to become an accomplished musician and highly-skilled performer in several arenas (Classical Pianist, Composer, Accompanist, Vocalist, Percussionist and Music Director). Gregory attained his Bachelor’s degree in Music Performance from the University of Central Florida. Currently, he is accompanying vocal and instrumental soloists as well as choral and instrumental ensembles for recitals, performance assessments, church services and recordings at home in Central Florida.
+                            Gregory Monroe II a MERN Full-Stack Developer, who got his education from the Georgia Institute of Technology. Previous to that, Gregory attained his Bachelor’s degree in Music Performance from the University of Central Florida. He now has been playing the piano for 25 years. 
                         </strong></p>
                     </div>
                 </div>
